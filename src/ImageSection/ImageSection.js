8ba3e008819eea556f415b5cb4906b6fa0f33fd9
Tomek <tomek1989gw@wppl>
2021-04-12 07:39:52 +0200

    loader p2

--- src/ImageSection/ImageSection.js
+++ src/ImageSection/ImageSection.js
@@ -1,15 +1,86 @@
-import React from 'react'
+import React/*, { useState }*/ from 'react'
 import './ImageSection.scss'
 import './ImageSection_RWD.scss'
 import './ImageSection_Ani.scss'
+import { Loader2 } from '../Loader2';
+
+
+
+let f;
+
+setTimeout( () => {
+    const ani = document.getElementsByClassName("animationLoader2")[0];
+    ani.classList.add("hidden");
+
+}, 1000 )
+
+
+setTimeout( () => {
+
+
+
+
+    f = setInterval( () => {
+        const defSquars = document.getElementsByClassName("defaultSquare");
+        const fady = document.getElementsByClassName("faded");
+
+        for (let index = 0; index < fady.length; index++) {
+            const element = fady[index];
+            element.classList.remove("defSquare");
+        }
+
+        const squares = document.getElementsByClassName("defSquare");
+
+        let testLength;
+        if(squares.length > 10) {
+            testLength = 10;
+        } else {
+            clearInterval(f);
+            for (let index = 0; index < defSquars.length; index++) {
+                const element = defSquars[index];
+                element.classList.remove("defSquare");
+                element.classList.add("faded");
+            }
+            return;
+        }
+
+        const test = parseInt(Math.floor(squares.length / testLength));
+
+        let rands = [];
+        for(let i = 0; i < testLength - 1; i++) {
+            const rand = Math.floor(Math.random() * (test * i)) + i;
+            rands.push(rand);
+        }
+
+        for (let index = 0; index < rands.length; index++) {
+            const element = rands[index];
+            if(squares[element]) {
+                squares[element].classList.add("faded");
+            }
+        }
+
+    }, 100);
+
+}, 3000);
+
 
 export const ImageSection = () => {
+
+    //const [square, setSquare] = useState(200);
     return (
         <section className="hero" id="hero">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
 
             <div>
                 <img className="imgi" src="img/brightPhoto2.jpg" alt="nic" />
+            </div>
+            <div className="animationLoader2">
+                <Loader2 />
+            </div>
+            <div className="animationSquares">
+                {Array(200).fill(0).map( (item, idx) => {
+                    return <div key={idx} className="defSquare defaultSquare"></div>;
+                } )}
             </div>
 
             <div className="container">
---
+++ src/Loader2.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Loader2.scss'
+
+export const Loader2 = () => {
+
+    return (
+        <div className="lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
